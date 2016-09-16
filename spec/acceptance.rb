075d960ae0deb2a4dc4fab1bafc856bf447bbc97
Josh Cheek <josh.cheek@gmail.com>
2016-09-15 23:56:49 -0500

    Refine the acceptance test and get the example program parseable

--- spec/acceptance.rb
+++ spec/acceptance.rb
@@ -1,4 +1,5 @@
 require 'open3'
+require 'biolangual'
 
 RSpec.describe 'acceptance test: examples/word_count' do
   let(:program_path) { File.expand_path '../examples/word_count.bio', __dir__ }
@@ -11,14 +12,22 @@ RSpec.describe 'acceptance test: examples/word_count' do
     Open3.capture3(biolang_path, program_path, *argv)
   end
 
+  it 'can parse the example program' do
+    program = File.read(program_path)
+    ast = Biolangual.parse(program)
+    expect(ast[:type]).to eq :expression
+  end
+
   it 'prints an error when there is only one arg' do
     stdout, stderr, status = run([])
+    expect(stderr).to eq ""
     expect(stdout).to eq "You must provide an argument to count\n"
     expect(status.exitstatus).to eq 1
   end
 
   it 'counts and prints the sorted words and counts' do
     stdout, stderr, status = run(['the cat and the hat'])
+    expect(stderr).to eq ""
     expect(stdout).to eq "5\n"
     expect(status.exitstatus).to eq 0
   end
