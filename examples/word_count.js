f2ed66dd0bce5c491e45a1146c2649b5644e3e97
Josh Cheek <josh.cheek@gmail.com>
2016-09-16 10:52:08 -0500

    *sigh* on the train, I have all the pkgs, but NPM doesn't know how to use them w/o the internet, this is enough to start, though

---
+++ examples/word_count.js
@@ -0,0 +1,18 @@
+var count_words = function(str) {
+  var count = 0
+  str.split(" ").forEach(function(word) { count += 1 })
+  return count
+}
+
+
+// 0: node bin, 1: this filename, 2: our argv
+var argv = process.argv.slice(2)
+
+console.log(argv)
+
+if(argv.length > 0) {
+  console.log(count_words(argv[0]))
+} else {
+  console.log("You must provide an argument to count")
+  process.exit(1)
+}
---
+++ test/interpreter_test.js
@@ -0,0 +1,12 @@
+'use strict'
+
+var assert = require('assert')
+describe('whatev', function() {
+  it('is a test 1', function() {
+    assert(false)
+  })
+
+  it('is a test 2', function() {
+    assert(true)
+  })
+})
