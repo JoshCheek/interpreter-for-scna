7c077cfcf299a8b8cf209ebfda2122849888a8a4
Josh Cheek <josh.cheek@gmail.com>
2016-09-16 13:59:17 -0500

    A class to interpret it

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
 function inspect(obj) {
   console.dir(obj, {depth: 4, colors: true})
 }
+
+module.exports = (function() {
+  // variables https://curiosity-driven.org/private-properties-in-javascript
+
+  class Interpreter {
+    constructor() {
+    }
+
+    evaluate(ast) {
+      inspect(ast)
+    }
+  }
+  return Interpreter
+})()
