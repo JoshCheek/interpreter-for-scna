d5649b8a6c42444c4ab9c1a81a2b23dc818619f2
Josh Cheek <josh.cheek@gmail.com>
2016-09-17 08:28:29 -0700

    Remove implementation

--- interpreter.js
+++ interpreter.js
@@ -76,126 +76,65 @@ module.exports = (function() {
       this.frame().result = value
     }
 
-    evalEmptyStatement(ast) {
-    }
-
     constructor(deps) {
       this.jsnull = {name: 'null', type: 'null', value: null}
 
-      const arraySlice = {
-        type:         'function',
-        functionType: 'native',
-        name:         'slice',
-        body:         function(callee, args) {
-          const ary    = callee.value
-          const idx    = args[0].value
-          const sliced = ary.slice(idx)
-          return { type: 'array', jsprops:{}, value: sliced }
-        },
-      }
-
-      const argv = { name: 'argv', type: 'array',
-        value: deps.argv.map(s => ({type: 'string', value: s})),
-        jsprops: { slice: arraySlice },
-      }
-
-      const jsprocess = { name: 'process', type: 'object',
-        jsprops: { argv: argv }
-      }
-
-      this.jsglobal = {name: 'global', type: 'object',
-        jsprops: { process: jsprocess }
-      }
-      this.jsglobal.jsprops.global = this.jsglobal
-
       this.callstack = [{
-        self:   this.jsglobal,
+        self:   {},
         vars:   {},
         result: this.jsnull,
       }]
     }
 
     EvalProgram(ast) {
-      ast.body.forEach(child => this.evaluate(child))
+      p(ast)
+      throw("EvalProgram")
     }
 
     EvalVariableDeclaration(ast) {
-      ast.declarations.forEach((child)=> {
-        this.evaluate(child)
-      })
+      p(ast)
+      throw("EvalVariableDeclaration")
     }
 
     EvalVariableDeclarator(ast) {
-      const name  = extractName(ast.id)
-      const value = this.evaluate(ast.init)
-      if(this.callstack.length === 1) {
-        this.jsglobal.jsprops[name] = value
-      } else {
-        this.frame().vars[name] = value
-      }
+      p(ast)
+      throw("EvalVariableDeclarator")
     }
 
     EvalFunctionExpression(ast) {
-      this.setReturn({
-        name:         extractName(ast.id),
-        type:         'function',
-        functionType: 'ast',
-        ast:          ast.body,
-        params:       ast.params,
-      })
+      p(ast)
+      throw("EvalFunctionExpression")
     }
 
     EvalCallExpression(ast) {
-      const fn   = this.evaluate(ast.callee)
-      const args = ast.arguments.map(arg => this.evaluate(arg))
-      if(fn.functionType === 'nst') {
-        throw("handle ast functions")
-      } else if (fn.functionType === 'native') {
-        const callee = fn.callee || this.jsglobal
-        const result = fn.body(callee, args)
-        this.setReturn(result)
-      } else {
-        throw `Uhhh wat?`
-      }
+      p(ast)
+      throw("EvalCallExpression")
     }
 
     EvalMemberExpression(ast) {
-      const obj  = this.evaluate(ast.object)
-      const name = ast.property.name
-      let   prop = obj.jsprops[name]
-      if(prop.type === 'function') {
-        prop = Object.create(prop)
-        prop.callee = obj
-      }
-      this.setReturn(prop)
+      p(ast)
+      throw("EvalMemberExpression")
     }
 
     EvalIdentifier(ast) {
-      const name   = extractName(ast)
-      const stack  = this.callstack
-      let   result = this.jsnull
-      // THIS IS WRONG! it shouldn't look up the callstack,
-      // it should look in the enclosed variable scopes
-      for(let i=stack.length-1; 0 <= i; --i) {
-        let frame = stack[i]
-        result = frame.vars[name]
-        if(result) break
-      }
-      if(!result) result = this.jsglobal.jsprops[name]
-      if(!result) result = this.jsnull
-      this.setReturn(result)
+      p(ast)
+      throw("EvalIdentifier")
     }
 
     EvalLiteral(ast) {
       let result
       if(typeof ast.value === "boolean") {
-        result = {type: "boolean", value: ast.value}
+        p(ast)
+        throw("EvalLiteral")
       } else if(typeof ast.value === "number") {
-        result = {type: "number", value: ast.value}
+        p(ast)
+        throw("EvalLiteral")
       } else if(typeof ast.value === "string") {
-        result = {type: "string", value: ast.value}
+        p(ast)
+        throw("EvalLiteral")
       } else if(ast.value === null) {
-        result = this.jsnull
+        p(ast)
+        throw("EvalLiteral")
       } else {
         p(ast)
         throw(`Whut? ${ast}`)
@@ -204,62 +143,47 @@ module.exports = (function() {
     }
 
     EvalExpressionStatement(ast) {
-      const expr = this.evaluate(ast.expression)
-      this.setReturn(expr)
+      p(ast)
+      throw("EvalExpressionStatement")
     }
 
     EvalBinaryExpression(ast) {
-      const operator = ast.operator
-      const left     = this.evaluate(ast.left)
-      const right    = this.evaluate(ast.right)
       if(operator === "+") {
-        const result = left.value + right.value
-        this.setReturn({type: "number", value: result})
+        p(ast)
+        throw("EvalBinaryExpression")
       } else if(operator === "<") {
-        const result = left.value < right.value
-        this.setReturn({type: "boolean", value: result})
+        p(ast)
+        throw("EvalBinaryExpression")
       } else if(operator === ">") {
-        const result = left.value > right.value
-        this.setReturn({type: "boolean", value: result})
+        p(ast)
+        throw("EvalBinaryExpression")
       } else if(operator === "===") {
-        const result = left.value === right.value
-        this.setReturn({type: "boolean", value: result})
+        p(ast)
+        throw("EvalBinaryExpression")
       } else {
         throw `Add a binary operator for: ${operator}`
       }
     }
 
     EvalIfStatement(ast) {
-      const condition = this.evaluate(ast.test)
-      if(condition.value) {
-        this.setReturn(this.evaluate(ast.consequent))
-      } else if(ast.alternate) {
-        this.setReturn(this.evaluate(ast.alternate))
-      }
+      p(ast)
+      throw("EvalIfStatement")
     }
 
     EvalAssignmentExpression(ast) {
-      // { type: 'AssignmentExpression',
-      //   operator: '=',
-      //   left: { type: 'Identifier', name: 'a' },
-      //   right: { type: 'Literal', value: 2, raw: '2' } }
-      const name  = extractName(ast.left)
-      const value = this.evaluate(ast.right)
-      this.frame().vars[name] = value
+      p(ast)
+      throw("EvalAssignmentExpression")
     }
 
 
     EvalBlockStatement(ast) {
-      ast.body.forEach(expr => this.evaluate(expr))
+      p(ast)
+      throw("EvalBlockStatement")
     }
 
     EvalEmptyStatement(ast) {
+      // noop
     }
-  }
-
-  function extractName(identifierAst) {
-    if(!identifierAst) return null
-    return identifierAst.name
   }
 
   return Interpreter
