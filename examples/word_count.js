fcbd602cde0527a58dd819b83ee4d6347cd549c3
Josh Cheek <josh.cheek@gmail.com>
2016-10-03 15:45:13 -0500

    bin/jss passes in the output stream, add an example for it, update style in word count example

--- examples/word_count.js
+++ examples/word_count.js
@@ -1,4 +1,4 @@
-var count_words = function(str) {
+var countWords = function(str) {
   var count = 0
   str.split(" ").forEach(function(word) { count = count + 1 })
   return count
@@ -11,7 +11,7 @@ var argv = process.argv.slice(2)
 console.log(argv)
 
 if(argv.length > 0) {
-  console.log(count_words(argv[0]))
+  console.log(countWords(argv[0]))
 } else {
   console.log("You must provide an argument to count")
   process.exit(1)
